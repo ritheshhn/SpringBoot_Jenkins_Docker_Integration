4f087424a1e211cf4eda84097299acca12eec332
RitheshNagaraj <rithesh.nagaraj@thantrick.in>
2024-07-27 17:13:29 +0530

    initial Commit with build final-name

---
+++ src/main/java/com/thantrick/Springboot_Jenkins_Docker_Demo/SpringbootJenkinsDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thantrick.Springboot_Jenkins_Docker_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thantrick/Springboot_Jenkins_Docker_Demo/SpringbootJenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thantrick.Springboot_Jenkins_Docker_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
